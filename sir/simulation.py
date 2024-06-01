663bbad0506332a3ed5ef29cc31ea94ae0e9930f
flo-schu <fluncki@protonmail.com>
2024-06-01 20:44:54 +0200

    Implement simulation replay speed change

--- sir/simulation.py
+++ sir/simulation.py
@@ -1,9 +1,35 @@
 import numpy as np
+from datetime import datetime, timedelta
 from matplotlib import pyplot as plt
+from matplotlib.widgets import Button, Slider
+
+from classroom import Classroom
 
 from boxes import draw_boxes
 
-def run(iteration, room, pupils, t_end, dt):
+class PlayPause:
+    long_pause = True
+
+    def _pause(self, event):
+        self.long_pause = True
+
+    def _play(self, event):
+        self.long_pause = False
+
+class Playspeed:
+    pause = 0.1
+
+    def update(self, val):
+        self.pause = val
+
+
+def run(
+    iteration: callable, 
+    room: Classroom, 
+    pupils: list, 
+    t_end: datetime, 
+    dt: timedelta
+):
 
     # make sure all concentration is going to zero 
     room.air_the_room(duration=np.inf)
@@ -15,6 +41,24 @@ def run(iteration, room, pupils, t_end, dt):
     room.concentration[0,0] = 100
     cax = ax.matshow(room.concentration, cmap='cool')
 
+    # add interactive buttons
+    # callback = PlayPause()
+    # axpause = fig.add_axes([0.7, 0.05, 0.1, 0.075])
+    # axplay = fig.add_axes([0.81, 0.05, 0.1, 0.075])
+    # bpause = Button(axpause, 'Pause')
+    # bpause.on_clicked(callback._pause)
+    # bplay = Button(axplay, 'Play')
+    # bplay.on_clicked(callback._play)
+        
+
+    # add interactive slider
+    callback = Playspeed()
+    axplayspeed = fig.add_axes([0.2, 0.5, 0.6, 0.02])
+    splayspeed = Slider(
+        ax=axplayspeed, label="Pause [s]", 
+        valmin=0.001, valmax=2, valinit=0.5, valstep=0.001
+    )
+    splayspeed.on_changed(callback.update)
     # plot to track the developemnt of infection over time.
 
     room.concentration[0,0] = 0
@@ -30,6 +74,7 @@ def run(iteration, room, pupils, t_end, dt):
         cax.set_data(room.concentration)
         ax.set_title(f"Zeit: {room.daymap[room.time.isoweekday()]}, {room.time}")
 
+        plt.pause(callback.pause)
     
     fig.colorbar(cax)
     ax.set_title("Virus Concentration")
